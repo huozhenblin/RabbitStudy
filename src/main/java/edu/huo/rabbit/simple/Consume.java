4e27d42abf805b9673508c73f43bc037252789cf
huodada <1015428987@qq.com>
2021-06-18 14:43:33 +0800

    init project

---
+++ src/main/java/edu/huo/rabbit/simple/Consume.java
@@ -0,0 +1,10 @@
+package edu.huo.rabbit.simple;
+
+/**
+ * @package: edu.huo.rabbit.simple
+ * @author: HuoZL
+ * @date: Create in 5:20 下午 2021/6/17
+ * @description:
+ */
+public class Consume {
+}
